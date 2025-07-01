d1deec12b95a6250642d54bf194e19ef571b0170
sabin-vv <vvsabin@gmail.com>
2025-07-01 22:07:59 +0530

    functon func updated

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get ("/",(req,res) => {
-    res.send("server Running")
-})
 
-function func(a,b){
+    function func(a,b){
     return a+b
 }
+    const result  = func(2,4)
+    console.log(result)
+    res.send(`result ${result}`)
+})
 
-const result  = func(2,4)
-res.send("result" ,result)
 
 app.listen(8000,() => {
     console.log("server started")
