f2351366f3814645bf72957a91d5c06c3a49c858
sabin-vv <vvsabin@gmail.com>
2025-07-01 21:59:00 +0530

    function func added

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ app.get ("/",(req,res) => {
     res.send("server Running")
 })
 
+function func(a,b){
+    return a+b
+}
+
+const result  = func(2,4)
+res.send("result" ,result)
+
 app.listen(8000,() => {
     console.log("server started")
 })
