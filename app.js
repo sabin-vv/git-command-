230616b1e44f839059ea851e21ff18fae4d0b6b5
sabin-vv <vvsabin@gmail.com>
2025-07-01 21:44:58 +0530

    intialize server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get ("/",(req,res) => {
+    res.send("server Running")
+})
+
+app.listen(8000,() => {
+    console.log("server started")
+})
